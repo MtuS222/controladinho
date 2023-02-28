277aab4d4699e0d70985cb114c4071d79ad0a753
Mateus Henrique Miguel Mariano <65180480+MtuS222@users.noreply.github.com>
2023-02-27 21:07:36 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/controladinho/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.controladinho
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.yellow,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final altura = MediaQuery.of(context).size.height;
+    final largura =MediaQuery.of(context).size.width;    
+    return Scaffold(
+      body: Row(
+        children: [
+         
+           Container(
+            height: altura,
+            width: largura,
+              color: const Color(0xff020475),
+              child: Image.network(
+                'https://cdn-icons-png.flaticon.com/128/9789/9789398.png',
+                height: 200,
+                width: 200,
+              ),
+            ),
+          
+        
+           Container(color: Colors.white),
+        
+        ],
+      ),
+    );
+  }
+}
