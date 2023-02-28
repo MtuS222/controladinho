82f99ae4ea490572b93b220d4ecf2718846da808
Bruno Almeida de Matos <65208006+BrunoAlm@users.noreply.github.com>
2023-02-27 23:16:09 -0300

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,17 @@ class MeuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
-        colorSchemeSeed: Colors.yellow,
+        colorSchemeSeed: Colors.blue,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              // padding: const EdgeInsets.all(20),
+              fixedSize: const Size(110, 30),
+              backgroundColor: const Color(0xff030475),
+              foregroundColor: Colors.white),
+        ),
       ),
       home: const LoginPage(),
     );
@@ -24,23 +32,101 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final altura = MediaQuery.of(context).size.height;
-    final largura = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Row(
         children: [
-          Container(
-            height: altura,
-            width: largura,
-            color: const Color(0xff030475),
-            child: Image.network(
-              'https://cdn-icons-png.flaticon.com/128/9789/9789398.png',
-              height: 200,
-              width: 200,
+          Expanded(
+            flex: 2,
+            child: Container(
+              alignment: Alignment.center,
+              color: const Color(0xff030475),
+              child: Image.network(
+                'https://cdn-icons-png.flaticon.com/128/9789/9789398.png',
+                fit: BoxFit.contain,
+                height: 400,
+              ),
             ),
           ),
-          Container(color: Colors.white),
+          Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(37.3),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(
+                      color: const Color(0xff030475),
+                    ),
+                  ),
+                  child: const LoginMenu(),
+                ),
+              ),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class LoginMenu extends StatelessWidget {
+  const LoginMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(80.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(
+            'https://www.ondaagil.com.br/configuracoes/7b4e637043d2e7caac092f6d650ccb19.png',
+          ),
+          const SizedBox(height: 70),
+          const MeuTextFormField(hintText: 'Usuário'),
+          const SizedBox(height: 20),
+          const MeuTextFormField(obscureText: true, hintText: 'Senha'),
+          const SizedBox(height: 80),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: () {}, child: const Text('Entrar')),
+              const SizedBox(width: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  'Cadastrar',
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MeuTextFormField extends StatelessWidget {
+  const MeuTextFormField({
+    super.key,
+    this.obscureText = false,
+    required this.hintText,
+  });
+  final bool obscureText;
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        hintText: hintText,
+        enabledBorder: const OutlineInputBorder(),
+        focusedBorder: const OutlineInputBorder(),
       ),
     );
   }
